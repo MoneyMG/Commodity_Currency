262816faa32de0fadb2bb45fe24fa5a3f3390f25
Mitch Greer <magreer@ualberta.ca>
2025-02-14 02:49:21 -0700

    bug

--- app/ui.R
+++ app/ui.R
@@ -61,8 +61,8 @@ ui <- tagList(
     tabPanel("Implications",
              br(),
              plotlyOutput('panel_vis'),
-             br(),
-             uiOutput("panel_write")),
+             br()),
+             #uiOutput("panel_write")),
     tabPanel("Use Cases", "This panel is intentionally left blank")
   )
 )
